751749bc78538dabc04a5a814270e11f51feaf15
Marius <95219519+MariusAGP@users.noreply.github.com>
2024-04-10 21:08:01 +0200

    form done

--- src/app/feature/donation/donation.module.ts
+++ src/app/feature/donation/donation.module.ts
@@ -7,6 +7,13 @@ import {ReactiveFormsModule} from "@angular/forms";
 import {MatSlideToggle} from "@angular/material/slide-toggle";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatOption, MatSelect} from "@angular/material/select";
+import {MatRadioButton, MatRadioGroup} from "@angular/material/radio";
+import {MatInput} from "@angular/material/input";
+import {MatIcon} from "@angular/material/icon";
+import {MatIconButton} from "@angular/material/button";
+import {MatDatepicker, MatDatepickerInput, MatDatepickerToggle} from "@angular/material/datepicker";
+import {provideNativeDateAdapter} from "@angular/material/core";
+import {MatDivider} from "@angular/material/divider";
 
 
 @NgModule({
@@ -21,7 +28,19 @@ import {MatOption, MatSelect} from "@angular/material/select";
     MatSlideToggle,
     MatFormFieldModule,
     MatSelect,
-    MatOption
+    MatOption,
+    MatRadioGroup,
+    MatRadioButton,
+    MatInput,
+    MatIcon,
+    MatIconButton,
+    MatDatepickerToggle,
+    MatDatepicker,
+    MatDatepickerInput,
+    MatDivider,
+  ],
+  providers: [
+    provideNativeDateAdapter()
   ]
 })
 export class DonationModule { }
