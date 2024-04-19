60b6064bddc6fdb419eb5094a2232272731c2f3f
Marius <95219519+MariusAGP@users.noreply.github.com>
2024-04-19 10:45:10 +0200

    new font and translation

--- src/app/feature/donation/donation.module.ts
+++ src/app/feature/donation/donation.module.ts
@@ -2,7 +2,6 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {DonateComponent} from "./donate/donate.component";
 import {DonationConfirmationComponent} from "./donation-confirmation/donation-confirmation.component";
-import {DonationHistoryComponent} from "./donation-history/donation-history.component";
 import {ReactiveFormsModule} from "@angular/forms";
 import {MatSlideToggle} from "@angular/material/slide-toggle";
 import {MatFormFieldModule} from "@angular/material/form-field";
@@ -21,7 +20,6 @@ import {MatProgressBar} from "@angular/material/progress-bar";
   declarations: [
     DonateComponent,
     DonationConfirmationComponent,
-    DonationHistoryComponent,
   ],
     imports: [
         CommonModule,
